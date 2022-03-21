a6fc40c7b45ad04d74c31a3c47f1f427b6a047a9
Jonathan Bergson <contato@bergson.me>
2022-03-21 15:34:11 -0300

    chore: novo design pattern Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton player01 = Singleton.Instance;
+            player01.Message("Jogador 1: a bola está comigo no meio do campo.");
+
+            Singleton player02 = Singleton.Instance;
+            player02.Message("Jogador 2: recebeu a bola.");
+
+            Singleton player03 = Singleton.Instance;
+            player03.Message("Jogador 3: recebeu o lançamento na linha de fundo.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null; 
+
+        public static Singleton Instance {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                    Console.WriteLine("Bola do Jogo...");
+                }
+
+                return instance;
+            }
+        }
+
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
